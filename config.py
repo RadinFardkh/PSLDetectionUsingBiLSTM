2d2431d4b3a48d8664192e1294e61aa63e57bbf0
Radin Fard Khojaste <radin.fkhojaste.sch@gmail.com>
2026-08-20 18:45:32 +0330

    Refactor config.py for clarity and organization
    Removed unused variables and organized sections.

--- config.py
+++ config.py
@@ -1,34 +1,28 @@
 import os
 
-# ========== Paths ==========
+# Paths
 DATA_DIR = "data"  # folder with class subfolders
 PROCESSED_DIR = "processed"  # where .npy files go
 MODEL_DIR = "models"    # where models go bruh
-LOG_DIR = "logs"
 
-# ========== Preprocessing ==========
-USE_HOLISTIC = True  # True = MediaPipe Holistic (pose+hands), False = Hands only
+# Preprocessing
 MAX_FRAMES = None  # keep all frames, or set to a fixed number (e.g., 60)
 SEQUENCE_LENGTH = 60  # for padded training, fixed length (pads/truncates)
-FEATURE_TYPE = "both"  # "angles", "coords", or "both"
 
 # Normalisation for coordinates (if used)
 COORD_NORM = "wrist"  # "wrist" (hand translation) or "shoulders" (body)
 # For angles, no extra normalisation needed
 
 
-# ========== Training ==========
-MODEL_TYPE = "bilstm"  # "bilstm" or "transformer" / "bilstm" is the recommended
+# Training
 BATCH_SIZE = 32
-EPOCHS = 200     # 60 for small database, 100 for bigger
+EPOCHS = 200
 LEARNING_RATE = 1e-3
 WEIGHT_DECAY = 1e-4
 DROPOUT = 0.3
 REDUCE_LR_FACTOR = 0.5
 REDUCE_LR_PATIENCE = 7
 EARLY_STOPPING_PATIENCE = 20    # stop after x epochs if no loss
-
-# BiLSTM specific
 LSTM_UNITS = 128    # no need for 64 unless you have worm
 LSTM_DROPOUT = DROPOUT  # the default dropout
 
@@ -38,10 +32,10 @@ NUM_HEADS = 4
 NUM_LAYERS = 2
 TRANSFORMER_DROPOUT = 0.2
 
-# ========== TFLite Export ==========
-QUANTIZE = True
+# Train and TFLite Exporting
+KERAS_MODEL_NAME = 'final_model.keras'
 TFLITE_MODEL_NAME = "psl_model.tflite"
 
-# ========== Misc ==========
+# Other
 SEED = 42   # random, 42 is just popular
 NUM_CLASSES = 33  # TODO: should change after any increase in database
